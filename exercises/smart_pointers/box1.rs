d4651bd368ad95404a6774f595587d5bec03be23
eeezds <2425863684@qq.com>
2024-09-26 20:03:10 +0800

    bb

--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -1,28 +1,6 @@
-// box1.rs
-//
-// At compile time, Rust needs to know how much space a type takes up. This
-// becomes problematic for recursive types, where a value can have as part of
-// itself another value of the same type. To get around the issue, we can use a
-// `Box` - a smart pointer used to store data on the heap, which also allows us
-// to wrap a recursive type.
-//
-// The recursive type we're implementing in this exercise is the `cons list` - a
-// data structure frequently found in functional programming languages. Each
-// item in a cons list contains two elements: the value of the current item and
-// the next item. The last item is a value called `Nil`.
-//
-// Step 1: use a `Box` in the enum definition to make the code compile
-// Step 2: create both empty and non-empty cons lists by replacing `todo!()`
-//
-// Note: the tests should not be changed
-//
-// Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -30,16 +8,16 @@ fn main() {
     println!("This is an empty cons list: {:?}", create_empty_list());
     println!(
         "This is a non-empty cons list: {:?}",
-        create_non_empty_list()
+        create_non_empty_list(10, create_empty_list())
     );
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    List::Nil
 }
 
-pub fn create_non_empty_list() -> List {
-    todo!()
+pub fn create_non_empty_list(head: i32, tail: List) -> List {
+    List::Cons(head, Box::new(tail))
 }
 
 #[cfg(test)]
@@ -48,11 +26,11 @@ mod tests {
 
     #[test]
     fn test_create_empty_list() {
-        assert_eq!(List::Nil, create_empty_list())
+        assert_eq!(List::Nil, create_empty_list());
     }
 
     #[test]
     fn test_create_non_empty_list() {
-        assert_ne!(create_empty_list(), create_non_empty_list())
+        assert_ne!(create_empty_list(), create_non_empty_list(1, create_empty_list()));
     }
 }
